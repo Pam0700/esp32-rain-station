865e6e3657cd2a2103c3b9096db773c055025f19
Pam0700 <duc98cnsh@gmail.com>
2026-01-01 18:07:43 +0700

    Update Esp32basic_code.ino
    Tinh gọn hơn

--- Esp32basic_code.ino
+++ Esp32basic_code.ino
@@ -1,13 +1,42 @@
-#define LED_PIN 2 // Khai báo chân code
+#include <Arduino.h>     // Thư viện mặc định (ESP32 thường có sẵn)
 
+#define CHAN_DEN 2       // Chân GPIO 2 nối LED
+#define CHAN_NUT 15      // Chân GPIO 15 nối nút nhấn
+int trangThaiNut = 0;        // Trạng thái đọc từ nút
+int trangThaiNutCu = 0;      // Trạng thái nút lần trước
+bool trangThaiDen = false;  // Đèn đang bật hay tắt
+bool trangThaiNutTruoc = HIGH; // Kiểm tra trạng tháu nút trước
+unsigned long thoiGianNhan = 0;
+unsigned long doTreChongDoi = 50; // ms
 void setup() {
-  pinMode(LED_PIN, OUTPUT); // Khai báo chân 
-  // tín hiệu đầu ra
-}
+  pinMode(CHAN_DEN, OUTPUT);
+  pinMode(CHAN_NUT, INPUT_PULLUP);
 
+  Serial.begin(115200);
+  Serial.println("ESP32 bat dau hoat dong");
+}
 void loop() {
-  digitalWrite(LED_PIN, HIGH); // LED ON
-  delay(1000);                 // chờ 1 giây
-  digitalWrite(LED_PIN, LOW);  // LED OFF
-  delay(1000);                 // chờ 1 giây
+  docNutNhan();
+  xuLyBatTatDen();
+}
+// Xử lí riêng lẻ
+void docNutNhan() {
+  int giaTriDoc = digitalRead(CHAN_NUT);
+
+  if (giaTriDoc != trangThaiNutCu) {
+    thoiGianNhan = millis();
+  }
+
+  if (millis() - thoiGianNhan > doTreChongDoi) {
+    trangThaiNut = giaTriDoc;
+  }
+
+  trangThaiNutCu = giaTriDoc;
+}
+void xuLyBatTatDen() {
+  if (trangThaiNut == LOW && trangThaiNutTruoc == HIGH) {y
+    trangThaiDen = !trangThaiDen;
+    digitalWrite(CHAN_DEN, trangThaiDen);
+  }
+  trangThaiNutTruoc = trangThaiNut;
 }
